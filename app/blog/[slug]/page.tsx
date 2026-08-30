7c031249ea53060a2c6434a38a4563dbb01b4581
666Dynamic <dynamichdarts@gmail.com>
2026-08-30 14:38:28 +0200

    Fix server-only MDX build

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -125,7 +125,6 @@ export default async function BlogPostPage({ params }: BlogPostPageProps) {
             "@type": "Organization",
             "name": "SprowadzoneAuto.pl"
         },
-        "description": post.excerpt,
         "articleBody": post.content
     }
 
